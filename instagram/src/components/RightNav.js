3df605445fdac31d0c202592750909ac4566a49c
amarmahdi <amarmehdi1112@gmail.com>
2021-08-28 22:55:44 +0300

    Initial commit

---
+++ instagram/src/components/RightNav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const RightNav = () => {
+  return (
+    <div
+      class="pl-80 pr-80 w-full flex fixed justify-end"
+      style={{ paddingTop: "84px" }}
+    >
+      <div
+        class="border-gray-300 border-solid h-screen pb-10"
+        style={{ borderWidth: "1px", width: "35.5%" }}
+      ></div>
+    </div>
+  );
+};
---
+++ instagram/src/components/Story.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Story = () => {
+  return (
+    <div
+      class="pl-80 pr-80 w-full flex relative"
+      style={{ paddingTop: "84px" }}
+    >
+      <div
+        class="border-gray-300 border-solid"
+        style={{ borderWidth: "1px", width: "64.5%", height: "118px" }}
+      ></div>
+    </div>
+  );
+};
