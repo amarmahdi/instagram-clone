79836c8f51ee33cc2f6600805f770aafcedf7463
amarmahdi <amarmehdi1112@gmail.com>
2021-09-08 18:26:58 +0300

    Comment Section added

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -4,22 +4,6 @@ import { Story } from "./components/Story";
 import { RightNav } from "./components/RightNav";
 import NewsFeed from "./components/NewsFeed";
 import "./App.css";
-// import NewsFeed from "./components/NewsFeed";
-
-// const imageSize = (imgUrl) =>
-//   new Promise((resolve, reject) => {
-//     let img = new Image();
-//     img.src = imgUrl;
-//     let size = img.height;
-//     imgUrl ? resolve(size) : reject(null);
-//   });
-
-// const a = (imgUrl) => {
-//   let img = new Image();
-//   img.src = imgUrl;
-//   let size = img.height;
-//   return size;
-// };
 
 const App = () => {
   return (
