11c370953ed17b9f1f9d0388b8f7a88bb0094228
amarmahdi <amarmehdi1112@gmail.com>
2021-09-09 17:37:57 +0300

    comment section Started

--- instagram/src/components/Feed.js
+++ instagram/src/components/Feed.js
@@ -117,17 +117,18 @@ export const Feed = (prop) => {
         }}
       >
         <div
-          className="flex justify-between"
+          className="flex justify-between pl-4 pr-4 mt-1"
           style={{
             height: "40px",
             border: "1px solid rgba(var(--b6a,219,219,219),1)",
             borderTop: "hidden",
             borderLeft: "hidden",
             borderRight: "hidden",
+            borderBottom: "hidden",
           }}
         >
           <div className="flex">
-            <div>
+            <div className="w-10 pt-1 justify-center">
               <svg
                 aria-label="Like"
                 class="_8-yf5 "
@@ -140,7 +141,7 @@ export const Feed = (prop) => {
                 <path d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>
               </svg>
             </div>
-            <div>
+            <div className="w-10 pt-1">
               <svg
                 aria-label="Comment"
                 class="_8-yf5 "
@@ -157,7 +158,7 @@ export const Feed = (prop) => {
                 ></path>
               </svg>
             </div>
-            <div>
+            <div className="w-10 pt-1">
               <svg
                 aria-label="Share Post"
                 class="_8-yf5 "
@@ -172,7 +173,7 @@ export const Feed = (prop) => {
             </div>
           </div>
           <div>
-            <div>
+            <div className="w-10 pt-1">
               <svg
                 aria-label="Save"
                 class="_8-yf5 "
@@ -189,110 +190,110 @@ export const Feed = (prop) => {
         </div>
       </div>
     </div>
-    // <div
-    //   style={{
-    //     position: "absolute",
-    //     paddingTop: "18px",
-    //     height: `${
-    //       prop.height > prop.width
-    //         ? protrait(prop.height)
-    //         : landscape(prop.height)
-    //     }px`,
-    //     marginBottom: "60px",
-    //     border: "1px solid rgba(var(--b6a,219,219,219),1)",
-    //     width: "64.5%",
-    //   }}
-    // >
-    //   <div
-    //     className="h-16 flex justify-between p-4 border-solid border-gray-300 w-full"
-    //     style={{ borderBottomWidth: "1px" }}
-    //   >
-    //     <div className="avatar">
-    //       <div className="rounded-full w-10 h-10">
-    //         <img
-    //           src="http://daisyui.com/tailwind-css-component-profile-1@56w.png"
-    //           alt="PP"
-    //         />
-    //       </div>
-    //     </div>
-    //     <div className="">
-    //       <p>amar</p>
-    //     </div>
-    //   </div>
-    //   <div
-    //     className=" flex justify-between border-solid absolute border-gray-300 w-full"
-    //     style={{ borderBottomWidth: "1px", height: "100%" }}
-    //   >
-    //     {/* TODO height  */}
-    //     <img
-    //       className="resize  h-full"
-    //       src={prop.img}
-    //       alt="img"
-    //       style={{
-    //         width: "100%",
-    //         objectFit: "cover",
-    //       }}
-    //     ></img>
-    //   </div>
-    //   <div className="h-5"></div>
-    // </div>
-
-    // <div
-    //   className="pl-80 pr-80 w-full flex"
-    //   style={{
-    //     paddingTop: "18px",
-    //     height: `calc(${prop.height}px/5)`,
-    //     maxHeight: "80%",
-    //   }}
-    // >
-    //   <div
-    //     className="border-gray-300 flex"
-    //     style={{
-    //       width: "64.5%",
-    //       marginBottom: "50px",
-    //       borderRadius: "2.5px",
-    //     }}
-    //   >
-    //     <div className="flex flex-col w-full h-full relative">
-    //       <div
-    //         className="h-16 flex justify-between p-4 border-solid border-gray-300 w-full"
-    //         style={{ borderWidth: "1px" }}
-    //       >
-    //         <div className="avatar">
-    //           <div className="rounded-full w-10 h-10">
-    //             <img
-    //               src="http://daisyui.com/tailwind-css-component-profile-1@56w.png"
-    //               alt="PP"
-    //             />
-    //           </div>
-    //         </div>
-    //         <div className="">
-    //           <p>amar</p>
-    //         </div>
-    //       </div>
-    //       <div
-    //         className="grid card bg-base-300 place-items-center absolute rounded-none mt-16"
-    //         style={{ height: "100%" }}
-    //       >
-    //         <img
-    //           className="resize  h-full w-full"
-    //           src={prop.img}
-    //           alt="img"
-    //           style={{
-    //             width: "100%",
-    //             objectFit: "cover",
-    //           }}
-    //         ></img>
-    //       </div>
-    //       <div
-    //         className="grid h-32 card border-solid  border-gray-300 bg-black z-50 w-full place-items-center rounded-none"
-    //         style={{
-    //           borderWidth: "1px",
-    //           marginTop: `calc(${prop.height}px/6 + 10px)`,
-    //         }}
-    //       ></div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
+// <div
+//   style={{
+//     position: "absolute",
+//     paddingTop: "18px",
+//     height: `${
+//       prop.height > prop.width
+//         ? protrait(prop.height)
+//         : landscape(prop.height)
+//     }px`,
+//     marginBottom: "60px",
+//     border: "1px solid rgba(var(--b6a,219,219,219),1)",
+//     width: "64.5%",
+//   }}
+// >
+//   <div
+//     className="h-16 flex justify-between p-4 border-solid border-gray-300 w-full"
+//     style={{ borderBottomWidth: "1px" }}
+//   >
+//     <div className="avatar">
+//       <div className="rounded-full w-10 h-10">
+//         <img
+//           src="http://daisyui.com/tailwind-css-component-profile-1@56w.png"
+//           alt="PP"
+//         />
+//       </div>
+//     </div>
+//     <div className="">
+//       <p>amar</p>
+//     </div>
+//   </div>
+//   <div
+//     className=" flex justify-between border-solid absolute border-gray-300 w-full"
+//     style={{ borderBottomWidth: "1px", height: "100%" }}
+//   >
+//     {/* TODO height  */}
+//     <img
+//       className="resize  h-full"
+//       src={prop.img}
+//       alt="img"
+//       style={{
+//         width: "100%",
+//         objectFit: "cover",
+//       }}
+//     ></img>
+//   </div>
+//   <div className="h-5"></div>
+// </div>
+
+// <div
+//   className="pl-80 pr-80 w-full flex"
+//   style={{
+//     paddingTop: "18px",
+//     height: `calc(${prop.height}px/5)`,
+//     maxHeight: "80%",
+//   }}
+// >
+//   <div
+//     className="border-gray-300 flex"
+//     style={{
+//       width: "64.5%",
+//       marginBottom: "50px",
+//       borderRadius: "2.5px",
+//     }}
+//   >
+//     <div className="flex flex-col w-full h-full relative">
+//       <div
+//         className="h-16 flex justify-between p-4 border-solid border-gray-300 w-full"
+//         style={{ borderWidth: "1px" }}
+//       >
+//         <div className="avatar">
+//           <div className="rounded-full w-10 h-10">
+//             <img
+//               src="http://daisyui.com/tailwind-css-component-profile-1@56w.png"
+//               alt="PP"
+//             />
+//           </div>
+//         </div>
+//         <div className="">
+//           <p>amar</p>
+//         </div>
+//       </div>
+//       <div
+//         className="grid card bg-base-300 place-items-center absolute rounded-none mt-16"
+//         style={{ height: "100%" }}
+//       >
+//         <img
+//           className="resize  h-full w-full"
+//           src={prop.img}
+//           alt="img"
+//           style={{
+//             width: "100%",
+//             objectFit: "cover",
+//           }}
+//         ></img>
+//       </div>
+//       <div
+//         className="grid h-32 card border-solid  border-gray-300 bg-black z-50 w-full place-items-center rounded-none"
+//         style={{
+//           borderWidth: "1px",
+//           marginTop: `calc(${prop.height}px/6 + 10px)`,
+//         }}
+//       ></div>
+//     </div>
+//   </div>
+// </div>
